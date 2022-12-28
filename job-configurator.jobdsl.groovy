f3b72a1791e50f141b7c8f162b5485209d33dc93
shubham030899 <119936821+shubham030899@users.noreply.github.com>
2022-12-28 11:08:52 +0530

    Update job-configurator.jobdsl.groovy

--- job-configurator.jobdsl.groovy
+++ job-configurator.jobdsl.groovy
@@ -1,3 +1,15 @@
-job('job-configurator') {
-descriprion('hello')
-}
+pipelineJob("job-configurator") {
+    properties {
+        disableConcurrentBuilds()
+        durabilityHint {
+            hint('PERFORMANCE_OPTIMIZED')
+        }
+        pipelineTriggers {
+            triggers {
+                gitlab {
+                    triggerOnPush(true)
+                    triggerOnMergeRequest(false)
+                }
+            }
+        }
+    }
